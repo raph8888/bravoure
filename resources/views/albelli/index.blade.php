62be633b9b2ca97d40c8df7b996eabf81dc788d4
raphaelsrodrigues <raph@raph-web.com>
2017-05-13 15:05:26 +0200

    adding form

--- resources/views/albelli/index.blade.php
+++ resources/views/albelli/index.blade.php
@@ -1,1 +1,43 @@
 <p>Hello World</p>
+
+
+<form action="/action_page.php">
+
+<div class="title">
+
+    <input type="text" name="title" />
+
+</div>
+
+
+<div class="content">
+
+    <input type="text" name="content" />
+
+
+</div>
+
+
+<div class="image">
+
+    <input type="image" name="image" />
+
+
+</div>
+
+
+<div class="email">
+
+    <input type="email" name="password" />
+
+
+</div>
+
+<div class="submit">
+
+<input type="submit" value="Submit">
+
+
+</div>
+
+</form>
